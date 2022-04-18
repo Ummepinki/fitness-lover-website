50fc4ca2cec9f186ed80c41db8b4b3ee328f0b2d
Ummepinki <salmaumme888@gmail.com>
2022-04-18 15:31:58 +0600

    banner added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import logo from '../../../images/logo.jpg';
+
 const Header = () => {
     return (
 
-        <div>
-            <h1> Heading</h1>
-        </div>
+        <>
+            <Navbar collapseOnSelect expand="lg" bg="dark" sticky='top' variant="dark">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">
+                        <img height={30} src={logo} alt="" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="home#services">Services</Nav.Link>
+                            <Nav.Link as={Link} to="home#blogs">Blogs</Nav.Link>
 
+                        </Nav>
+                        <Nav>
+                            <Nav.Link as={Link} to="about">About</Nav.Link>
+
+                            <Nav.Link as={Link} to="login">
+                                Login
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+
+        </>
 
 
     );
