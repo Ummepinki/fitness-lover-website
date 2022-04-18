703d56743c872557a17bf0933ef3f3a63d489208
Ummepinki <salmaumme888@gmail.com>
2022-04-18 11:12:35 +0600

    fake data added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 
-import { Route } from 'react-router-dom';
-import './App.css';
+
+
+import { Route, Routes } from 'react-router-dom';
+import About from './Pages/About/About';
 import Home from './Pages/Home/Home/Home';
+import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Route path="/" element={<Home></Home>}></Route>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+      </Routes>
+      <Footer></Footer>
+
     </div>
   );
 }
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../../../images/logo.jpg';
 const Header = () => {
     return (
-        <div>
 
+        <div>
+            <h1> Heading</h1>
         </div>
+
+
+
     );
 };
 
