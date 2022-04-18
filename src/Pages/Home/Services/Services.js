1d4752d0deeb2c4dc7e2a28875da43cb9ce1684b
Ummepinki <salmaumme888@gmail.com>
2022-04-18 13:29:35 +0600

    navbar added

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
+import './Services.css';
 
 const Services = () => {
     const [services, setServices] = useState([]);
@@ -11,7 +13,17 @@ const Services = () => {
 
     return (
         <div>
-            <h2>Services:{services.length}</h2>
+            <h2 className='services-title'> Services Section</h2>
+
+            <div className='services-container'>
+                {
+                    services.map(service => <Service
+                        key={service.id}
+                        service={service}
+                    ></Service>)
+                }
+            </div>
+
         </div>
     );
 };
